724b64f5262b7dd1a14cce9d2a05293528a82d95
Ge Tan <gtan@me.com>
2015-12-07 09:30:18 +0100

    Make it into package. Add util methods

--- R/fastq.R
+++ R/fastq.R
@@ -1,2 +1,21 @@
-
+### -----------------------------------------------------------------
+### get the fn of the paired reads
+### Exported!
+getPairedReads <- function(reads1){
+  filePairs <- data.frame(reads1=c("R1_001.fastq.gz", "R1.fastq.gz", 
+                                   "R1.fastq", "F3.csfasta", "F3.csfasta"),
+                          reads2=c("R2_001.fastq.gz", "R2.fastq.gz", 
+                                   "R2.fastq", "F5-BC.csfasta", 
+                                   "F5-RNA.csfasta"))
+  for (i in 1:nrow(filePairs)){
+    x <- filePairs$reads1[i]
+    if (my.grep(x, reads1)){
+      reads2 <- sub(x, filePairs$reads2[i], reads1)
+      if(file.exists(reads2)){
+        return(reads2)
+      }
+    }
+  }
+  return(NULL)
+}
 
---
+++ R/util.R
@@ -0,0 +1,22 @@
+### -----------------------------------------------------------------
+### grep for a patternList with the combination of "or" or "and"
+### Exported!
+
+grepPatternList <- function(patternList, x, combine=c("or", "and"), ...){
+  combine <- match.arg(combine)
+  result <- logical(length(x))
+  if (combine == "or"){
+    for (pattern in patternList){
+      idx <- grep(pattern, x, ...)
+      result[idx] <- TRUE
+    }
+  } else if (combine == "and"){
+    result[] <- TRUE
+    for (pattern in patternList){
+      idx <- grep(pattern, x, ...)
+      result[-idx] <- FALSE
+    }
+  }
+  result
+}
+
