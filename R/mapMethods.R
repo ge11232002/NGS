c031a53754cad816ec5faa7e5494ceef9666f7a0
Ge Tan <gtan@me.com>
2016-01-13 20:31:42 +0000

    start bwaMem

--- R/mapMethods.R
+++ R/mapMethods.R
@@ -10,6 +10,15 @@ mapBWA <- function(){
   
 }
 
+### -----------------------------------------------------------------
+### BWA-MEM algorithm for 70bp-1Mbp query sequences
+### 
+bwaMem <- function(reads1, output, ref, opt="", mc.cores=getThreads(),
+                   binary="bwa"){
+  
+  
+}
+
 mapTophat <- function(){
   
 }
